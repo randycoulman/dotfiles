643cee4a455ed257e350efec905f90dd7f3e8947
Randy Coulman <rcoulman@gmail.com>
2013-05-31 16:36:02 -0700

    Switch to built-in electric modes instead of ruby-electric.

--- emacs.d/init.d/key-bindings.el
+++ emacs.d/init.d/key-bindings.el
@@ -1,4 +1,5 @@
-(global-set-key "" (quote newline-and-indent))
-(global-set-key (kbd "RET") (quote newline-and-indent))
 (global-set-key "" (quote undo))
 (global-set-key "\C-ci" 'idomenu)
+
+(electric-indent-mode t)
+(electric-pair-mode t)
--- emacs.d/init.d/ruby.el
+++ emacs.d/init.d/ruby.el
@@ -1,4 +1,3 @@
-(require 'ruby-electric)
 (require 'rcodetools)
 (require 'ruby-compilation)
 
@@ -8,12 +7,7 @@
 (autoload 'inf-ruby-keys "inf-ruby"
   "Set local key defs for inf-ruby in ruby-mode")
 
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
-
 (add-hook 'ruby-mode-hook 'inf-ruby-keys)
-(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("[Rr]akefile\\'" . ruby-mode))
