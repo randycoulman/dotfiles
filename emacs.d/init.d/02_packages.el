6e76b1915f37082141ae6e68458da8a92ed15d78
Randy Coulman <rcoulman@gmail.com>
2013-11-05 07:43:10 -0800

    Use pallet to manage packages.

--- emacs.d/init.d/02_packages.el
+++ emacs.d/init.d/02_packages.el
@@ -1,26 +1,3 @@
-(package-initialize)
-
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(setq rac-required-packages
-      (list 'exec-path-from-shell
-            'expand-region
-            'idomenu
-            'iedit
-            'inf-ruby
-            'markdown-mode
-            'mmm-mode
-            'multi-term
-            'multiple-cursors
-            'ruby-compilation
-            'smartparens
-            'yari
-            'yasnippet))
-
-(dolist (package rac-required-packages)
-  (when (not (package-installed-p package))
-    (package-refresh-contents)
-    (package-install package)))
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
