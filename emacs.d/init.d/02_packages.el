a0d4bee863ac73652af6d46e0fe18039df5e0d65
Randy Coulman <rcoulman@gmail.com>
2013-11-05 07:11:49 -0800

    Add multiple-cursors.

--- emacs.d/init.d/02_packages.el
+++ emacs.d/init.d/02_packages.el
@@ -14,6 +14,7 @@
             'markdown-mode
             'mmm-mode
             'multi-term
+            'multiple-cursors
             'ruby-compilation
             'smartparens
             'yari
--- emacs.d/init.d/editing.el
+++ emacs.d/init.d/editing.el
@@ -1,3 +1,14 @@
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this-dwim)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
 (require 'smartparens-config)
 
 ;;; "Borrowed" from emacsredux.com and Ryan Davis (aka @zenspider)
