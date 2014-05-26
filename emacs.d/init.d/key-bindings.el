166dc04023e9114899b9da427c82c1d8c81c7207
Randy Coulman <rcoulman@gmail.com>
2014-05-26 09:42:29 -0700

    Use smex

--- emacs.d/init.d/key-bindings.el
+++ emacs.d/init.d/key-bindings.el
@@ -1,3 +1,6 @@
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (global-set-key "" 'newline-and-indent)
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key "" (quote undo))
