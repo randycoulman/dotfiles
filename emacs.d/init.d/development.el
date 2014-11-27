b70139740856c724e8778484eeb5aac1e2ee6e61
Randy Coulman <rcoulman@gmail.com>
2014-11-26 17:16:03 -0800

    Don't set the fill-column in programming modes

--- emacs.d/init.d/development.el
+++ emacs.d/init.d/development.el
@@ -6,9 +6,6 @@
 
 (which-function-mode)
 
-(add-hook 'prog-mode-hook
-          '(lambda ()
-             (set-fill-column 80)))
 (add-hook 'prog-mode-hook 'auto-fill-mode)
 (add-hook 'prog-mode-hook 'subword-mode)
 
